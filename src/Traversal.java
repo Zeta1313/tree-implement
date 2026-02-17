734cd8027fdd24c8f48db8b3c1db944c02785725
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2026-02-17 14:36:40 -0800

    2/17 class

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,36 +1,60 @@
 public class Traversal {
   public static void main(String[] args) {
-    TreeNode root = new TreeNode(10, null, null);
-    root.left = new TreeNode(9, null, null);
-    root.left.left = new TreeNode(5, null, null);
-    root.left.right = new TreeNode(2, null, null);
-    root.right = new TreeNode(15, null, null);
-    root.right.left = new TreeNode(-3, null, null);
-    root.right.right = new TreeNode(5, null, null);
-    root.right.right.right = new TreeNode(22, null, null);
+    TreeNode<String> Stringroot = new TreeNode<String>("lol", null, null);
+    Stringroot.left = new TreeNode<String>("Laugh", null, null);
+    Stringroot.right = new TreeNode<String>("Chuckle", null, null);
+
+    TreeNode<Integer> root = new TreeNode<Integer>(10, null, null);
+    root.left = new TreeNode<Integer>(9, null, null);
+    root.left.left = new TreeNode<Integer>(5, null, null);
+    root.left.right = new TreeNode<Integer>(2, null, null);
+    root.right = new TreeNode<Integer>(15, null, null);
+    root.right.left = new TreeNode<Integer>(-3, null, null);
+    root.right.right = new TreeNode<Integer>(5, null, null);
+    root.right.right.right = new TreeNode<Integer>(22, null, null);
 
     preOrder(root);
-    System.out.println("-------------");
+    System.out.println();
     postOrder(root);
-    System.out.println("-------------");
+    System.out.println();
     inOrder(root);
+    System.out.println();
+    inOrder(Stringroot);
+    System.out.println();
+    preOrderGreater(root);
+    System.out.println();
+    System.out.println(countNodes(root));
+    System.out.print(countNodes(Stringroot));
   }
-  public static void preOrder(TreeNode node){
+  public static void preOrder(TreeNode<?> node){
     if (node == null) return;
-    System.out.println(node.value);
+    System.out.print(node.value);
     preOrder(node.left);
     preOrder(node.right);
   }
-  public static void postOrder(TreeNode node){
+  public static void postOrder(TreeNode<?> node){
     if (node == null) return;
     postOrder(node.left);
     postOrder(node.right);
-    System.out.println(node.value);
+    System.out.print(node.value + " ");
   }
-  public static void inOrder(TreeNode node){
+  public static void inOrder(TreeNode<?> node){
     if (node == null) return;
     inOrder(node.left);
-    System.out.println(node.value);
+    System.out.print(node.value + " ");
     inOrder(node.right);
+  }
+  public static void preOrderGreater(TreeNode<Integer> node){
+    if (node == null) return;
+    if (node.value > 7) {
+      System.out.print(node.value + " ");
+    }
+    preOrderGreater(node.left);
+    preOrderGreater(node.right);
+  }
+  public static int countNodes(TreeNode<?> node) {
+    if (node == null) return 0;
+    if (node.right == null && node.left == null) return 1;
+    return countNodes(node.right) + countNodes(node.left) + 1;
   }
 }
